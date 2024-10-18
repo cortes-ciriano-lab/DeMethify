a79ba9e225022a16702eefe6f8fff965d8b708b6
Marwane Bourdim <79879340+MarwaneBourdim@users.noreply.github.com>
2024-10-18 04:56:45 +0100

    Update feature_selection.py

--- preprocessing/feature_selection.py
+++ preprocessing/feature_selection.py
@@ -12,17 +12,17 @@ def feature_select_var(bedfile, n, output_folder):
         print(f'Creating directory {output_folder} to store results')
         os.mkdir(output_folder)
       
-    top_n_rows.to_csv(output_folder + "/select_ref.bed", sep='\t', header=True, index=False)
+    top_n_rows.to_csv(output_folder + "/"+ bedfile[:-4] + "_select_ref.bed", sep='\t', header=True, index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Select top N rows with highest variance from a BED file.')
     
     parser.add_argument('bedfile', type=str, help='Path to the input BED file')
     parser.add_argument('n', type=int, help='Number of top rows to select')
-    parser.add_argument('out', nargs='?', type=str, default=".", help='Path to output folder')
+    parser.add_argument('output_folder',nargs='?', type=str, default=".", help='Path to output folder')
 
     args = parser.parse_args()
 
     
     
-    feature_select_var(args.bedfile, args.n, args.out)
+    feature_select_var(args.bedfile, args.n, args.output_folder)

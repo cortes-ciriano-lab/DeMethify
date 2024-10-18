d4424dda9117ae1fdc367a1bc435358ec581b692
Marwane Bourdim <79879340+MarwaneBourdim@users.noreply.github.com>
2024-10-18 07:25:33 +0100

    Update demethify.py

--- demethify/demethify.py
+++ demethify/demethify.py
@@ -534,9 +534,9 @@ def main():
         else:
             counts = np.ones_like(meth_f)
 
-    print(args.methfreq)
-    args.methfreq = [bla.split("/")[-1] for bla in args.methfreq]
-    print(args.methfreq)
+    # print(args.methfreq)
+    # args.methfreq = [bla.split("/")[-1] for bla in args.methfreq]
+    # print(args.methfreq)
         
     # deconvolution
     time_start = time()

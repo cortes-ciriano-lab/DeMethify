ee0758e1324444205d845f43de9a6d155e672cd9
Marwane Bourdim <79879340+MarwaneBourdim@users.noreply.github.com>
2024-10-23 21:16:10 +0100

    Update demethify.py

--- demethify/demethify.py
+++ demethify/demethify.py
@@ -29,7 +29,7 @@ def main():
     # Add regular arguments
     parser.add_argument('--methfreq', nargs='+', type=str, required=True, help='Methylation frequency file path (values between 0 and 1)')
     parser.add_argument('--ref', nargs='?', type=str, help='Methylation reference matrix file path')
-    parser.add_argument('--iterations', nargs=2, type=int, help='Numbers of iterations for outer and inner loops (default = 10000, 20)')
+    parser.add_argument('--iterations', nargs=2, type=int, help='Numbers of iterations for outer and inner loops (default without purity = 10000, 20, with purity= 100, 500)')
     parser.add_argument('--nbunknown', nargs=1, type=int, help="Number of unknown cell types to estimate ")
     parser.add_argument('--purity', nargs='+', type=int, help="The purity of the sample in percent [0,100], if known")
     parser.add_argument('--termination', nargs=1, type=float, default=1e-2, help='Termination condition for cost function (default = 1e-2)')

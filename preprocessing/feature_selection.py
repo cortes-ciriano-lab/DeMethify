76f7b4680fb682b99c4cfb9b87dea3f5a497bcfe
Marwane Bourdim <79879340+MarwaneBourdim@users.noreply.github.com>
2024-11-25 00:59:47 +0000

    Update feature_selection.py

--- preprocessing/feature_selection.py
+++ preprocessing/feature_selection.py
@@ -11,15 +11,17 @@ def feature_select_var(bedfile, n, output_folder):
     if not os.path.exists(output_folder):
         print(f'Creating directory {output_folder} to store results')
         os.mkdir(output_folder)
+        
+    output_file = bed_file[:-4].split("/")[-1] + "_select_ref.bed"
       
-    top_n_rows.to_csv(output_folder + "/"+ bedfile[:-4] + "_select_ref.bed", sep='\t', header=True, index=False)
+    top_n_rows.to_csv(output_folder + "/"+ output_file, sep='\t', header=True, index=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Select top N rows with highest variance from a BED file.')
     
-    parser.add_argument('bedfile', type=str, help='Path to the input BED file')
-    parser.add_argument('n', type=int, help='Number of top rows to select')
-    parser.add_argument('output_folder',nargs='?', type=str, default=".", help='Path to output folder')
+    parser.add_argument('--bedfile', type=str, help='Path to the input BED file')
+    parser.add_argument('--n', type=int, help='Number of top rows to select')
+    parser.add_argument('--out',nargs='?', type=str, default=".", help='Path to output folder')
 
     args = parser.parse_args()
 

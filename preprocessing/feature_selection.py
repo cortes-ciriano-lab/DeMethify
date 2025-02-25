7ba9a8d85e0cf37866c7e14c86ea699270054083
Marwane Bourdim <79879340+MarwaneBourdim@users.noreply.github.com>
2025-02-25 22:22:21 +0000

    Update feature_selection.py

--- preprocessing/feature_selection.py
+++ preprocessing/feature_selection.py
@@ -1,30 +1,37 @@
 import pandas as pd
 import os
 import argparse
+import numpy as np
+from scipy.linalg import svd
 
-def feature_select_var(bedfile, n, output_folder):
+def feature_select(bedfile, n, output_folder, method="var"):
     df = pd.read_csv(bedfile, sep='\t')
-    
-    row_variances = df.iloc[:, 3:].var(axis=1)
-    top_n_rows = df.loc[row_variances.nlargest(n).index]
+    df_cleaned = df.dropna()
+
+    if method == "var":
+        row_variances = df_cleaned.iloc[:, 3:].var(axis=1)
+        selected_rows = df_cleaned.loc[row_variances.nlargest(n).index]
+    elif method == "svd":
+        A = df_cleaned.iloc[:, 3:].values
+        U, _, _ = svd(A, full_matrices=False)
+        row_scores = np.abs(U[:, :n]).sum(axis=1)
+        selected_rows = df_cleaned.iloc[np.argsort(-row_scores)[:n]]
+    else:
+        raise ValueError("Invalid method! Choose 'var' or 'svd'.")
 
     if not os.path.exists(output_folder):
-        print(f'Creating directory {output_folder} to store results')
-        os.mkdir(output_folder)
-        
-    output_file = bedfile[:-4].split("/")[-1] + "_select_ref.bed"
-      
-    top_n_rows.to_csv(output_folder + "/"+ output_file, sep='\t', header=True, index=False)
+        os.makedirs(output_folder, exist_ok=True)
+
+    output_file = os.path.join(output_folder, os.path.basename(bedfile).replace(".bed", "_select_ref.bed"))
+    selected_rows.to_csv(output_file, sep='\t', header=True, index=False)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Select top N rows with highest variance from a BED file.')
-    
-    parser.add_argument('--bed', type=str, help='Path to the input BED file')
-    parser.add_argument('--n', type=int, help='Number of top rows to select')
-    parser.add_argument('--out',nargs='?', type=str, default=".", help='Path to output folder')
+    parser = argparse.ArgumentParser(description='Select top N rows using variance or SVD from a BED file.')
+    parser.add_argument('--bed', type=str, required=True, help='Path to the input BED file')
+    parser.add_argument('--n', type=int, required=True, help='Number of top rows to select')
+    parser.add_argument('--out', nargs='?', type=str, default=".", help='Path to output folder')
+    parser.add_argument('--method', type=str, choices=["var", "svd"], default="var")
 
     args = parser.parse_args()
+    feature_select(args.bed, args.n, args.out, args.method)
 
-    
-    
-    feature_select_var(args.bed, args.n, args.out)
